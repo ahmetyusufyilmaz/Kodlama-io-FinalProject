e8884253f5f88741bc2dc9d86fca3a6fd632522e
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2022-01-24 19:35:43 +0300

    DTOs created

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,14 +10,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CategoryTest();
+            ProductTest();
+                
+        }
 
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
-            { 
-                Console.WriteLine(product.ProductName);
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName+"/" +product.CategoryName);
             }
-
         }
     }
 }
