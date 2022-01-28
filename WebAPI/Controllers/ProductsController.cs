3245164d7eee626c0bc05281705b2b682b63e4f2
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2022-01-28 17:52:57 +0300

    Added a new product with json
    Added a new product to the database with json using swagger and postman.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -22,14 +22,44 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]
+        [HttpGet("getall")]
 
-        public List<Product> Get()
+        public IActionResult GetAll()
         {
            
             var result= _productService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result); 
         }
+
+        [HttpGet("getbyid")]
+
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
 
     }
 }
